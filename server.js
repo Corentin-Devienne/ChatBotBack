7a7042161796f287d61d9a01e56289f9a5ad226b
Corentin-Devienne <117612198+Corentin-Devienne@users.noreply.github.com>
2023-05-11 16:44:17 +0200

    update

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/',(req,res)) => {
+app.get('/',(req,res) => {
     res.send('hello world')
-}
+})
 
-app.listen(port, {}) => {
-    console.log("Example app listenning on port $(port")
-}
+app.listen(port,() => {
+    console.log("Example app listenning on port $(port)")
+})
     
 
